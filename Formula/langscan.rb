b777b8530e7c1d069f5f8865c21e72ee6c683525
zakka <mzakka163@gmail.com>
2026-02-08 22:06:41 +0700

    fix: correct macOS Intel URL and update SHA256 sums for macOS and Linux builds

--- Formula/langscan.rb
+++ Formula/langscan.rb
@@ -4,18 +4,18 @@ class Langscan < Formula
   version "0.1.0"
 
   on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/Zakka163/homebrew-langscan/releases/download/v0.1.0/langscan-macos-arm64"
-      sha256 "ISI_SHA256_ARM"
+   if Hardware::CPU.intel?
+      url "https://github.com/Zakka163/langscan/releases/download/v0.1.0/langscan-macos-amd64"
+      sha256 "64090c35b0649ae73787fbea159fc86ba033769a92f80ccbc8d2369699da0d71"
     else
       url "https://github.com/Zakka163/homebrew-langscan/releases/download/v0.1.0/langscan-macos-amd64"
-      sha256 "ISI_SHA256_AMD"
+      sha256 "64090c35b0649ae73787fbea159fc86ba033769a92f80ccbc8d2369699da0d71"
     end
   end
 
   on_linux do
     url "https://github.com/Zakka163/homebrew-langscan/releases/download/v0.1.0/langscan-linux-amd64"
-    sha256 "ISI_SHA256_LINUX"
+    sha256 "1cf4a54700dd4db23bdb3eb06f73ceaf4e03827e3a34d49db2b394bfb7f20671"
   end
 
   def install
